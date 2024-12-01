8396697c8c8ca31286651aa64ddcde8f14aaf9d7
Jellypipo <43985275+Jellypipo@users.noreply.github.com>
2024-12-01 17:11:17 +0100

    update

--- model.py
+++ model.py
@@ -1,45 +1,45 @@
 import numpy as np
+import pandas as pd
+import pickle
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, LSTM, Embedding, Dense
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import pandas as pd
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
-# Load the dataset from a csv file
-data = pd.read_csv("SentencesFinal.csv")
+# Preprocessing function
+def preprocess_text(sentence):
+    return ''.join(char for char in sentence if char.isalnum() or char.isspace()).lower()
 
-# Separate the columns of sentences
-english_sentences = data['English'].tolist()
-german_sentences = data['German'].tolist()
-
-# Adding additional tokens for the german sentences
-german_sentences = ["<start> " + sentence + " <end>" for sentence in german_sentences]
-
+# Load the dataset from a CSV file
+data = pd.read_csv("data/Informal_Sentences.csv")
+english_sentences = data['English'].apply(preprocess_text).tolist()
+german_sentences = ["<start> " + preprocess_text(sentence) + " <end>" for sentence in data['German'].tolist()]
 
 # Parameters
-num_words = 1000
-max_sequence_length = 10
+num_words = 5000  # Increased vocabulary size
+max_sequence_length = 20  # Allow longer sequences
 
-# Tokenizer for english
-tokenizer_eng = Tokenizer(num_words=num_words, filters='')
+# Tokenizer for English
+tokenizer_eng = Tokenizer(num_words=num_words, filters='', oov_token='<unk>')
 tokenizer_eng.fit_on_texts(english_sentences)
 input_sequences = tokenizer_eng.texts_to_sequences(english_sentences)
-input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='post')
+input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='post', truncating='post')
 
-# Tokenizer for german
-tokenizer_ger = Tokenizer(num_words=num_words, filters='')
+# Tokenizer for German
+tokenizer_ger = Tokenizer(num_words=num_words, filters='', oov_token='<unk>')
 tokenizer_ger.fit_on_texts(german_sentences)
 target_sequences = tokenizer_ger.texts_to_sequences(german_sentences)
-target_sequences = pad_sequences(target_sequences, maxlen=max_sequence_length, padding='post')
+target_sequences = pad_sequences(target_sequences, maxlen=max_sequence_length, padding='post', truncating='post')
 
-# Getting size of vocabulary
+# Vocabulary sizes
 input_vocab_size = len(tokenizer_eng.word_index) + 1
 target_vocab_size = len(tokenizer_ger.word_index) + 1
 
 # Model parameters
-embedding_dim = 128
-latent_dim = 256
+embedding_dim = 256
+latent_dim = 512
 
 # Encoder
 encoder_inputs = Input(shape=(max_sequence_length,))
@@ -61,58 +61,46 @@ model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
-# Desplazar la secuencia de salida (y_train)
-# Shifting the target sequence
+# Shift the target sequences
 target_sequences_input = target_sequences[:, :-1]
 target_sequences_output = target_sequences[:, 1:]
-
-# Añadir dimensión extra para el formato (batch_size, sequence_length, 1)
-# Extra dimension for the format 
 target_sequences_output = np.expand_dims(target_sequences_output, axis=-1)
 
+# Early stopping and learning rate reduction
+early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5)
+
 # Training
-epochs = 100
-batch_size = 16
-print("Forma de input_sequences:", input_sequences.shape)
-print("Forma de target_sequences_input:", target_sequences_input.shape)
-print("Forma de target_sequences_output:", target_sequences_output.shape)
-
-# Use only the first 10000 samples
-input_sequences = input_sequences[:10000]
-target_sequences_input = target_sequences_input[:10000]
-target_sequences_output = target_sequences_output[:10000]
-
-"""
-dataset = tf.data.Dataset.from_tensor_slices((
-    (input_sequences, target_sequences_input),
-    target_sequences_output
-))
-
-dataset = dataset.shuffle(buffer_size=10000).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-
-history = model.fit(dataset, epochs=epochs, validation_split=0.2)
-
-"""
+epochs = 30  # Train for more epochs
+batch_size = 64  # Larger batch size for faster training
 history = model.fit(
     [input_sequences, target_sequences_input],
     target_sequences_output,
     batch_size=batch_size,
     epochs=epochs,
-    validation_split=0.2
-) 
+    validation_split=0.2,
+    callbacks=[early_stopping, reduce_lr]
+)
 
+# Save the trained model
+model.save('nmt_model.keras')
 
-# Modelo de inferencia para el encoder
-# Inference Model for the encoder
+# Save tokenizers using pickle
+with open('tokenizer_eng.pkl', 'wb') as f:
+    pickle.dump(tokenizer_eng, f)
+with open('tokenizer_ger.pkl', 'wb') as f:
+    pickle.dump(tokenizer_ger, f)
+
+# Inference Models
 encoder_model = Model(encoder_inputs, encoder_states)
 
-# Inference model for the decoder
 decoder_state_input_h = Input(shape=(latent_dim,))
 decoder_state_input_c = Input(shape=(latent_dim,))
 decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
 
+decoder_embedding_output = decoder_embedding(decoder_inputs)
 decoder_lstm_outputs, state_h, state_c = decoder_lstm(
-    decoder_embedding, initial_state=decoder_states_inputs)
+    decoder_embedding_output, initial_state=decoder_states_inputs)
 decoder_states = [state_h, state_c]
 decoder_outputs = decoder_dense(decoder_lstm_outputs)
 
@@ -121,15 +109,13 @@ decoder_model = Model(
     [decoder_outputs] + decoder_states
 )
 
-# Translate function given an input
+# Translate function
 def translate_sentence(input_text):
-    input_seq = tokenizer_eng.texts_to_sequences([input_text])
+    input_seq = tokenizer_eng.texts_to_sequences([preprocess_text(input_text)])
     input_seq = pad_sequences(input_seq, maxlen=max_sequence_length, padding='post')
 
-    # Obtener el estado del encoder
     states_value = encoder_model.predict(input_seq)
 
-    # Generar secuencia de inicio para el decoder
     target_seq = np.zeros((1, 1))
     target_seq[0, 0] = tokenizer_ger.word_index['<start>']
 
@@ -139,32 +125,18 @@ def translate_sentence(input_text):
     while not stop_condition:
         output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
 
-        # Obtener el token con mayor probabilidad
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
-        sampled_word = tokenizer_ger.index_word.get(sampled_token_index, '')
+        sampled_word = tokenizer_ger.index_word.get(sampled_token_index, '<unk>')
 
         if sampled_word == '<end>' or len(translated_sentence) > max_sequence_length:
             stop_condition = True
         else:
             translated_sentence.append(sampled_word)
 
-        # Actualizar la secuencia de entrada y los estados
         target_seq = np.array([[sampled_token_index]])
         states_value = [h, c]
 
     return ' '.join(translated_sentence)
 
-# Example
-print(translate_sentence("guten"))
-
-# Save model
-model.save('nmt_model.h5')
-
-# Import tokenizers
-import pickle
-with open('tokenizer_eng.pkl', 'wb') as f:
-    pickle.dump(tokenizer_eng, f)
-with open('tokenizer_ger.pkl', 'wb') as f:
-    pickle.dump(tokenizer_ger, f)
-
-
+# Example translation
+print(translate_sentence("hello, how are you today?"))
